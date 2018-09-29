60c04fa77af2dfcc797d1989b53b214b539c2acf
Przemmad <przemmad@wp.pl>
2018-09-29 14:51:00 +0200

    :zadanie 2 przycisk more

--- js/app.js
+++ js/app.js
@@ -10,6 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
   menuButton.addEventListener('mouseout', function() {
   subMenu.style.display = 'none';
 });
-  {console.log('hej!');
-});
+
+var buttons = document.querySelectorAll('.read-more');
+function showHide(){
+ var textArea = this.previousElementSibling;
+
+  if (textArea.style.display === 'none' || textArea.style.display === '') {
+       textArea.style.display = 'block';
+       this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+     } else {
+       textArea.style.display = 'none';
+       this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+     }
+   }
+   
+  for (var i = 0; i < buttons.length; i++) {
+       buttons[i].addEventListener('click', showHide);
+   }
 });
